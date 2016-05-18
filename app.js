278926ab5771acf23c6b965470f4648882026475
Dan Duffy <blakflagg@bitbucket.org>
2016-05-18 10:53:43 -0700

    added yargs lib; updated code to handle location args for zip or city

--- app.js
+++ app.js
@@ -1,25 +1,66 @@
 
 var weather = require('./weather');
 var location = require('./location.js');
+var argv = require('yargs')
+.command('location','location',function(yargs) {
+  yargs.options({
+      zip : {
+          demand : false,
+          alias : 'z',
+          description : 'zip code',
+          type : 'string'
+      },
+      city : {
+          demand : false,
+          alias : 'c',
+          description : 'city and country code e.g. \"San Fransico,us\"',
+          type : 'string'
+      }
+  }).help('help');
+})
+.help('help')
+.argv;
 
+var command = argv._[0];
 
-location(function(locData){
+if(command){
+    var zip = (typeof argv.zip != 'undefined' ? argv.zip : null)    
+    var city = (typeof argv.city != 'undefined' ? argv.city : null)
+    
+    
+    var locData = {
+        postal : zip,
+        city : city
+    }
+
+getWeather(locData);
+
+    
+}else{
+    location(function(locData){
     if(!locData){
         throw new Error('NO_DATA');
     }
     
     try {
-        weather(function(callback){
-         console.log(callback);
-        },locData)
-        //console.log(locData);
+        
+        getWeather(locData);
     } catch (error) {
         console.log('ERROR: ' + error.message);
     }
 
 });
+}
 
 
+function getWeather(location) {
+
+weather(location,function(weatherData){
+   console.log(weatherData);
+   
+  });
+  
+}
 
 
 
--- weather.js
+++ weather.js
@@ -2,11 +2,16 @@ var request = require('request');
 const APIKEY = 'c6f474d91d53177532ea5db97ba11bf1';
 const url = 'http://api.openweathermap.org/data/2.5/weather?units=imperial&APIKEY=' + APIKEY;
 
-module.exports = function(callback,locData){
-    
+module.exports = function(locData,callback){
+    var qString = '';
+    if(locData.postal){
+        qString = `&zip=${locData.postal},us`
+    }else if(locData.city){
+        qString = `&q=${locData.city}`
+    }
     
     request({
-    url: url + `&zip=${locData.postal},us`,
+    url: url + qString,
     json: true
     }, function(error, repsonse, body){
         
@@ -14,7 +19,7 @@ module.exports = function(callback,locData){
         callback('unable to fetch weather');
     }else{
 
-        callback("it's " + body.main.temp + " in " + locData.city);
+        callback("it's " + body.main.temp + " in " + body.name);
     }
     });
 }
